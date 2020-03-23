d307bf818464b23ed9a752554a8eda568683f4e5
newyork167 <newyork167@gmail.com>
2020-03-22 22:36:10 -0400

    Beta code working, needs more testing with more interesting code before merge

--- scripts/load_plugin.py
+++ scripts/load_plugin.py
@@ -7,7 +7,10 @@ parser.add_argument("-l", "--load", help="Load plugin", action="store_true", des
 parser.add_argument("-u", "--unload", help="Unload plugin", action="store_true", dest="plugin_unload")
 arguments = parser.parse_args()
 
+plugin_name = arguments.plugin_name.strip()
+print('{action} {plugin_name}'.format(action="Loading" if arguments.plugin_load else "Unloading", plugin_name=plugin_name))
+
 action = "loadPlugin" if arguments.plugin_load else "unloadPlugin"
-maya_socket.SendCommand('print("{action} plugin\\n")'.format(action="Loading" if arguments.plugin_load else "Unloading"))
-maya_socket.SendCommand('{action} "{plugin_name}"'.format(action=action, plugin_name=arguments.plugin_name))
+maya_socket.SendCommand('print("{action} plugin: {plugin_name}\\n")'.format(action="Loading" if arguments.plugin_load else "Unloading", plugin_name=plugin_name))
+maya_socket.SendCommand('{action} "{plugin_name}"'.format(action=action, plugin_name=plugin_name))
 
