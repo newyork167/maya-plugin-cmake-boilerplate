6a16e078abc6f10e6ea8b7eecab5e9134839dcef
newyork167 <newyork167@gmail.com>
2020-03-23 11:43:11 -0400

    Added datetime stamp to Maya and CMake output

--- scripts/load_plugin.py
+++ scripts/load_plugin.py
@@ -1,5 +1,8 @@
 import maya_socket
 import argparse
+import datetime
+
+now = datetime.datetime.now()
 
 parser = argparse.ArgumentParser(description='Load/Unload Maya Plugins.')
 parser.add_argument("-p", "--plugin_name", help="Plugin Name", required=True)
@@ -8,9 +11,9 @@ parser.add_argument("-u", "--unload", help="Unload plugin", action="store_true",
 arguments = parser.parse_args()
 
 plugin_name = arguments.plugin_name.strip()
-print('{action} {plugin_name}'.format(action="Loading" if arguments.plugin_load else "Unloading", plugin_name=plugin_name))
+print('{action} {plugin_name} @ {now_time}'.format(action="Loading" if arguments.plugin_load else "Unloading", plugin_name=plugin_name, now_time=now.strftime("%m/%d/%Y, %H:%M:%S")))
 
 action = "loadPlugin" if arguments.plugin_load else "unloadPlugin"
-maya_socket.SendCommand('print("{action} plugin: {plugin_name}\\n")'.format(action="Loading" if arguments.plugin_load else "Unloading", plugin_name=plugin_name))
+maya_socket.SendCommand('print("{action} plugin: {plugin_name} @ {now_time}\\n")'.format(action="Loading" if arguments.plugin_load else "Unloading", plugin_name=plugin_name, now_time=now.strftime("%m/%d/%Y, %H:%M:%S")))
 maya_socket.SendCommand('{action} "{plugin_name}"'.format(action=action, plugin_name=plugin_name))
 
